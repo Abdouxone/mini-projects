4e4b852c0416f67d308b3c1366af226f2ef05771
Abdou se <abdouselim.edu@gmail.com>
2026-04-23 21:11:42 +0100

    image-gallery

---
+++ app/image-gallery/page.tsx
@@ -0,0 +1,80 @@
+import { SearchIcon } from "lucide-react";
+import React from "react";
+
+const images = [
+  { id: 1, url: "/image1.jpg" },
+  { id: 2, url: "/image2.jpg" },
+  { id: 3, url: "/image3.jpg" },
+  { id: 4, url: "/image4.jpg" },
+  { id: 5, url: "/image5.jpg" },
+  { id: 6, url: "/image6.jpg" },
+];
+
+export default function page() {
+  return (
+    <div className="bg-cyan-200/30 min-h-screen flex justify-center items-center">
+      {/* container */}
+      <div className="flex flex-col md:w-full md:m-5 bg-white rounded-xl w-xs">
+        {/* NavBar */}
+        <div className="flex flex-col mt-10 mb-10 md:flex-row md:mr-30 md:space-y-0 md:justify-end md:gap-10 space-y-4 items-center ">
+          {/* Vector */}
+          <div className="flex flex-col group ">
+            <h2 className="text-black hover:cursor-pointer mb-2">Vector</h2>
+            <div className="group-hover:opacity-100 opacity-0 border-b-2 transition-all duration-300"></div>
+          </div>
+          {/* Illustrations */}
+          <div className="flex flex-col group">
+            <h2 className="text-black hover:cursor-pointer mb-2 ">
+              Illustrations
+            </h2>
+            <div className="group-hover:opacity-100 opacity-0 border-b-2   transition-all duration-300"></div>
+          </div>
+          {/* Images */}
+          <div className="flex flex-col group">
+            <h2 className="text-black hover:cursor-pointer  mb-2">Images</h2>
+            <div className="group-hover:opacity-100 opacity-0 border-b-2 transition-all duration-300"></div>
+          </div>
+          {/* Icons */}
+          <div className="flex flex-col group">
+            <h2 className="text-black hover:cursor-pointer mb-2">Icons</h2>
+            <div className="group-hover:opacity-100 opacity-0 border-b-2 transition-all duration-300"></div>
+          </div>
+        </div>
+        {/* Search and upload button */}
+        <div className="flex flex-col items-center md:justify-between w-[80%] mx-auto justify-center md:flex-row space-y-4 md:space-y-0">
+          {/* Search */}
+          <div className="flex border-b-2 w-full justify-between md:w-xs border-gray-500/40 ">
+            <input type="text" placeholder="Search" />
+            <SearchIcon className="opacity-50" />
+          </div>
+          {/* Button */}
+          <div className="bg-black  hover:cursor-pointer md:w-xs w-full hover:bg-white hover:text-black border transition-all duration-300 text-white md:p-4 p-4 rounded-xl text-center font-semibold">
+            Upload
+          </div>
+        </div>
+        {/* Images */}
+        <div className="flex mb-10 md:flex-row w-[80%] mx-auto  mt-10 md:space-x-5 flex-col  md:space-y-0 space-y-4">
+          {images.map((image) => (
+            <div className="group relative">
+              <img
+                key={image.id}
+                className="rounded-lg "
+                src={image.url}
+                alt="pic"
+              />
+              <div className="absolute text-white bg-black rounded-b-lg p-2 w-full bottom-0  opacity-0 group-hover:opacity-70 transition-all duration-300">
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-col">
+                    <span className="font-serif">Abstract Painting</span>
+                    <span className="text-xs">245 likes - 35 Shares</span>
+                  </div>
+                  <img className="w-6" src="/bookmark.svg" alt="" />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
