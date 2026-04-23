99f3fce0fd31d1fc9b0ff7984019c194ab771333
Abdou se <abdouselim.edu@gmail.com>
2026-04-23 22:33:37 +0100

    image-gallery

--- app/image-gallery/page.tsx
+++ app/image-gallery/page.tsx
@@ -14,28 +14,28 @@ export default function page() {
   return (
     <div className="bg-cyan-200/30 min-h-screen flex justify-center items-center">
       {/* container */}
-      <div className="flex flex-col md:w-full md:m-5 bg-white rounded-xl w-xs">
+      <div className="flex flex-col shadow-2xl md:w-full md:m-5 mt-3 bg-white rounded-xl w-xs">
         {/* NavBar */}
         <div className="flex flex-col mt-10 mb-10 md:flex-row md:mr-30 md:space-y-0 md:justify-end md:gap-10 space-y-4 items-center ">
           {/* Vector */}
-          <div className="flex flex-col group ">
+          <div className="group ">
             <h2 className="text-black hover:cursor-pointer mb-2">Vector</h2>
-            <div className="group-hover:opacity-100 opacity-0 border-b-2 transition-all duration-300"></div>
+            <div className="group-hover:opacity-100  opacity-0 border-b-2 transition-all duration-300"></div>
           </div>
           {/* Illustrations */}
-          <div className="flex flex-col group">
+          <div className=" group">
             <h2 className="text-black hover:cursor-pointer mb-2 ">
               Illustrations
             </h2>
             <div className="group-hover:opacity-100 opacity-0 border-b-2   transition-all duration-300"></div>
           </div>
           {/* Images */}
-          <div className="flex flex-col group">
+          <div className="group">
             <h2 className="text-black hover:cursor-pointer  mb-2">Images</h2>
             <div className="group-hover:opacity-100 opacity-0 border-b-2 transition-all duration-300"></div>
           </div>
           {/* Icons */}
-          <div className="flex flex-col group">
+          <div className=" group">
             <h2 className="text-black hover:cursor-pointer mb-2">Icons</h2>
             <div className="group-hover:opacity-100 opacity-0 border-b-2 transition-all duration-300"></div>
           </div>
@@ -44,7 +44,11 @@ export default function page() {
         <div className="flex flex-col items-center md:justify-between w-[80%] mx-auto justify-center md:flex-row space-y-4 md:space-y-0">
           {/* Search */}
           <div className="flex border-b-2 w-full justify-between md:w-xs border-gray-500/40 ">
-            <input type="text" placeholder="Search" />
+            <input
+              type="text"
+              className="focus:outline-none border-none"
+              placeholder="Search"
+            />
             <SearchIcon className="opacity-50" />
           </div>
           {/* Button */}
@@ -53,7 +57,7 @@ export default function page() {
           </div>
         </div>
         {/* Images */}
-        <div className="flex mb-10 md:flex-row w-[80%] mx-auto  mt-10 md:space-x-5 flex-col  md:space-y-0 space-y-4">
+        <div className="grid  mb-10 md:grid-cols-3 w-[80%] mx-auto  mt-10 md:space-x-5  md:space-y-3 space-y-4">
           {images.map((image) => (
             <div className="group relative">
               <img
@@ -62,7 +66,7 @@ export default function page() {
                 src={image.url}
                 alt="pic"
               />
-              <div className="absolute text-white bg-black rounded-b-lg p-2 w-full bottom-0  opacity-0 group-hover:opacity-70 transition-all duration-300">
+              <div className="absolute text-white bg-black rounded-b-lg p-2 w-full bottom-0  opacity-0 group-hover:opacity-60 transition-all duration-500">
                 <div className="flex flex-row justify-between">
                   <div className="flex flex-col">
                     <span className="font-serif">Abstract Painting</span>
