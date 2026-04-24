98bc2aad22043438541e721d6d363927e33ed657
Abdou se <abdouselim.edu@gmail.com>
2026-04-24 16:10:36 +0100

    finish the login modal

--- app/login-modal/page.tsx
+++ app/login-modal/page.tsx
@@ -3,8 +3,12 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-pink-500/15 flex items-center justify-center">
-      <div className="flex flex-col bg-white md:flex-row rounded-lg shadow-xl w-sm md:w-fit  md:h-152">
+    <div className="min-h-screen bg-rose-50 flex items-center justify-center">
+      <div className="flex relative flex-col bg-white md:flex-row rounded-lg shadow-xl w-sm md:w-fit  md:h-152">
+        {/* absolute element */}
+        <div className="absolute top-3 text-2xl text-white md:bg-white bg-black/50 rounded-full px-3 hover:scale-105 transition-all hover:cursor-pointer duration-300 md:text-black opacity-80  right-5">
+          X
+        </div>
         <div className="p-10 space-y-5 md:w-md">
           {/* log in */}
           <div className="space-y-4">
@@ -19,7 +23,7 @@ export default function page() {
             <input
               type="text"
               placeholder="Enter you Ermail address"
-              className="border w-full p-5  rounded border-zinc-300 placeholder:text-zinc-500/50"
+              className="border w-full p-5  rounded-lg border-zinc-300 placeholder:text-zinc-500/50"
             />
           </div>
 
@@ -28,7 +32,7 @@ export default function page() {
             <a href="" className="text-center text-cyan-600 ">
               Forgot Password
             </a>
-            <div className=" text-white text-center hover:shadow-xl duration-300  hover:-translate-y-1 transition-all flex flex-row items-center justify-center gap-5  bg-cyan-600 p-5 rounded">
+            <div className=" text-white text-center hover:shadow-xl duration-300  hover:-translate-y-1 transition-all flex flex-row items-center justify-center gap-5  bg-cyan-600 p-5 rounded-xl">
               <span className="text-lg font-semibold">Next</span>
               <ArrowRight color="white" />
             </div>
@@ -44,12 +48,12 @@ export default function page() {
           {/* facebool, gooogle */}
           <div className="flex flex-col md:justify-between md:flex-row gap-5">
             {/* Facebook */}
-            <div className="flex-row flex justify-center border p-2 rounded border-zinc-300 hover:-translate-y-1 transition-all duration-300 hover:shadow-xl items-center gap-4">
+            <div className="flex-row flex justify-center border px-5 py-3 rounded-2xl border-zinc-300 hover:-translate-y-1 transition-all duration-300 hover:shadow-xl items-center gap-4">
               <img src="/facebook.png" className="w-10" alt="" />
               <span className="text-zinc-700">Facebook</span>
             </div>
             {/* Google */}
-            <div className="flex-row flex justify-center border p-2 rounded border-zinc-300 hover:-translate-y-1 transition-all duration-300 hover:shadow-xl items-center gap-4">
+            <div className="flex-row flex justify-center border px-5 py-3 rounded-2xl border-zinc-300 hover:-translate-y-1 transition-all duration-300 hover:shadow-xl items-center gap-4">
               <img src="/google.png" className="w-10" alt="" />
               <span className="text-zinc-700">Google</span>
             </div>
