27dc4d72c63e2201fc7d30f26876172119442769
Abdou se <abdouselim.edu@gmail.com>
2026-05-02 16:38:06 +0100

    second section

--- app/abdoustudios/page.tsx
+++ app/abdoustudios/page.tsx
@@ -25,10 +25,36 @@ export default function page() {
             </nav>
           </div>
         </div>
-        <div className="max-w-6xl text-7xl mt-24 px-10 mx-auto">
-          <h1 className="max-w-lg tracking-wide text-white border-2 p-8">
+        <div className="max-w-6xl md:text-6xl text-4xl  mt-24 px-10 mx-auto">
+          <h1 className="max-w-lg uppercase font-thin tracking-wide text-left text-white border-2 pl-5 md:pl-12 pt-5 pb-5">
             Impressive Experiences That Deliver
           </h1>
+        </div>
+      </div>
+      {/* section 2 */}
+      <div className="md:max-w-6xl md:px-0 px-6  font-nunito mx-auto mt-30 mb-50">
+        <div className="flex relative flex-col md:flex-row space-y-10 md:space-y-0">
+          <img
+            className="w-190 hidden md:block"
+            src="/abdoustudios/images/desktop/image-interactive.jpg"
+            alt=""
+          />
+          <img
+            className=" block md:hidden"
+            src="/abdoustudios/images/mobile/image-interactive.jpg"
+            alt=""
+          />
+          <div className="md:absolute md:text-left text-center bottom-0 right-0 bg-white md:p-15">
+            <h1 className="text-5xl font-extralight max-w-md tracking-wide">
+              The leader in interactive VR
+            </h1>
+            <p className="md:max-w-md text-sm mt-5 leading-6 font-thin">
+              Founded in 2011, Loopstudios has been producing world-class
+              virtual reality projects for some of the best companies around the
+              globe. Our award-winning creations have transformed businesses
+              through digital experiences that bind to their brand.
+            </p>
+          </div>
         </div>
       </div>
     </div>
