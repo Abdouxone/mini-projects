e186b2e03d8ba7db53aa1fa37265bfb105e628c9
Abdou se <abdouselim.edu@gmail.com>
2026-05-04 19:16:16 +0100

    Adding links section

--- app/shortly/page.tsx
+++ app/shortly/page.tsx
@@ -32,9 +32,9 @@ export default function page() {
         </div>
       </div>
       {/* hero */}
-      <div className="max-w-7xl font-poppins  mx-auto">
-        <div className="flex  flex-col-reverse p-5 pt-2  justify-between   lg:flex-row">
-          <div className="max-w-md pt-20  text-center lg:text-left">
+      <div className="max-w-7xl mb-45 font-poppins  mx-auto">
+        <div className="flex  flex-col-reverse p-5 pt-2 items-center  justify-between   lg:flex-row">
+          <div className="lg:max-w-md max-w-xl pt-20  text-center lg:text-left">
             <h1 className="lg:text-6xl text-5xl  font-bold">
               More than just shorter links
             </h1>
@@ -55,6 +55,54 @@ export default function page() {
           </div>
         </div>
       </div>
+
+      {/* links section */}
+      <section className=" bg-gray-200 font-poppins ">
+        <div className="max-w-4xl p-5  mx-auto space-y-5">
+          <div className="bg-gray-800 -mt-19 justify-center md:space-x-10 md:space-y-0 space-y-5 items-center flex flex-col p-9 rounded-lg md:flex-row">
+            <input
+              type="text"
+              className=" border-none outline-none w-full p-3 text-black rounded-lg placeholder:text-orange-400 bg-white "
+              placeholder="Shorten a link here..."
+            />
+
+            <button className="px-12 py-3 w-full md:w-fit  text-white   bg-[#2BD0D0] rounded-lg whitespace-nowrap">
+              Shorten it!
+            </button>
+          </div>
+          <div className="flex flex-col  md:flex-row md:justify-between rounded-lg p-8 justify-center items-center bg-white">
+            <p className="text-black text-lg">https://frontendmentor.io</p>
+            <div className="flex flex-col md:flex-row justify-center items-center md:space-y-0 space-y-3 md:space-x-4">
+              <p className="text-[#2BD0D0] text-lg">https://rel.ink/k4IKyk</p>
+              <button className="bg-[#2BD0D0] text-white px-8 py-2 rounded-lg">
+                Copy
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row md:justify-between rounded-lg p-8 justify-center items-center bg-white">
+            <p className="text-black text-lg">
+              https://twitter.com/frontendmentor
+            </p>
+            <div className="flex flex-col md:flex-row justify-center items-center md:space-y-0 space-y-3 md:space-x-4">
+              <p className="text-[#2BD0D0] text-lg">https://rel.ink/gxOXp9</p>
+              <button className="bg-[#2BD0D0] text-white px-8 py-2 rounded-lg">
+                Copy
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row md:justify-between rounded-lg p-8 justify-center items-center bg-white">
+            <p className="text-black text-lg ">
+              https://linkedin.com/frontend-mentor
+            </p>
+            <div className="flex flex-col md:flex-row justify-center items-center md:space-y-0 space-y-3 md:space-x-4">
+              <p className="text-[#2BD0D0] text-lg">https://rel.ink/gob3X9</p>
+              <button className="bg-[#2BD0D0] text-white px-8 py-2 rounded-lg">
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
