d024610c5c595935cafb034f5aee3ca8e516a89f
Abdou se <abdouselim.edu@gmail.com>
2026-05-19 14:55:55 +0100

    Changing bg in countdown

--- app/countdown/page.tsx
+++ app/countdown/page.tsx
@@ -30,32 +30,41 @@ export default function page() {
 
   return (
     <div className="relative">
-      <div className="bg-[url('/countdown/oppenheimer.png')] absolute inset-0 min-h-screen  bg-cover bg-no-repeat"></div>
-      <div className="max-w-2xl mx-auto relative z-5 mt-20 flex flex-col items-center justify-center">
+      <div className="bg-[url('/countdown/van.jpg')]   absolute inset-0 min-h-screen  bg-center bg-cover bg-no-repeat"></div>
+      <div className="max-w-2xl mx-auto relative z-5 space-y-10 mt-20 flex flex-col items-center justify-center">
         <h1 className="text-4xl font-rubik text-white">
           We Are Coming Soon...
         </h1>
-        <p className="text-center">
+        <p className="text-center text-zinc-200">
           Start working with Techwind that can provide everything you need to
           generate awareness, drive traffic, connect.
         </p>
 
-        <div className="grid text-white md:grid-cols-4 gap-5 grid-cols-2 mt-10">
-          <div className="p-7 w-25 h-25 flex-col flex items-center justify-center bg-white/20 border border-white/20  shadow-2xl rounded-full">
-            <h1>{days}</h1>
+        <div className="grid font-poppins   text-white md:grid-cols-4 gap-5 grid-cols-2 mt-5">
+          <div className="p-7 w-25 h-25 flex-col flex items-center justify-center backdrop-blur-2xl backdrop-saturate-150 border border-white/20 shadow-[0_8px_32px_rgba(0,0,0,0.37)] rounded-full">
+            <h1 className="text-2xl">{days}</h1>
             <span>days</span>
           </div>
-          <div className="p-7 w-25 h-25 flex-col flex items-center justify-center shadow-2xl border border-white/20  bg-white/20 rounded-full">
-            <h1>{hours}</h1>
+          <div className="p-7 w-25 h-25 flex-col flex items-center justify-center backdrop-blur-2xl backdrop-saturate-150 border border-white/20 shadow-[0_8px_32px_rgba(0,0,0,0.37)] rounded-full">
+            <h1 className="text-2xl">{hours}</h1>
             <span>hours</span>
           </div>
-          <div className="p-7 w-25 h-25 flex-col flex items-center justify-cente  shadow-2xl border border-white/20  bg-white/20 rounded-full">
-            <h1>{minutes}</h1>
+          <div className="p-7 w-25 h-25 flex-col flex items-center justify-center  backdrop-blur-2xl backdrop-saturate-150 border border-white/20 shadow-[0_8px_32px_rgba(0,0,0,0.37)] rounded-full">
+            <h1 className="text-2xl">{minutes}</h1>
             <span>minutes</span>
           </div>
-          <div className="p-7 w-25 h-25 flex-col flex items-center justify-center  shadow-2xl border border-white/20 bg-white/20 rounded-full">
-            <h1>{seconds}</h1>
+          <div className="p-7 w-25 h-25 flex-col flex items-center justify-center backdrop-blur-2xl backdrop-saturate-150 border border-white/20 shadow-[0_8px_32px_rgba(0,0,0,0.37)] rounded-full">
+            <h1 className="text-2xl">{seconds}</h1>
             <span>seconds</span>
+          </div>
+        </div>
+
+        <div className="flex-row flex gap-5">
+          <div>
+            <img src="/clipboard/images/icon-facebook.svg" alt="" />
+          </div>
+          <div>
+            <img src="/clipboard/images/icon-facebook.svg" alt="" />
           </div>
         </div>
       </div>
