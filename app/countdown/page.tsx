0521db07144920e0a0ee6b0fe4af82bc21821a25
Abdou se <abdouselim.edu@gmail.com>
2026-05-20 17:03:43 +0100

    fix countdown in phone

--- app/countdown/page.tsx
+++ app/countdown/page.tsx
@@ -37,9 +37,10 @@ export default function page() {
       <div
         className={`max-w-2xl ${styles.container} px-5 md:px-0 mx-auto relative z-5 space-y-10 mt-10 md:mt-20 flex flex-col items-center justify-center`}
       >
-        <h1 className="md:text-4xl font-bold text-3xl max-w-xs text-center font-rubik text-white">
-          We Are Coming Soon...
-        </h1>
+        <div className="md:text-4xl font-bold text-3xl flex flex-col md:flex-row text-center font-rubik text-white">
+          <span>We Are</span>
+          <span>Coming Soon...</span>
+        </div>
         <p className="text-center font-rubik font-normal md:text-xl text-sm max-w-2xl text-zinc-200">
           You can't lift the stone without being ready for the snake that's
           revealed.
