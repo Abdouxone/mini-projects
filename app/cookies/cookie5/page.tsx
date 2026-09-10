0369d2ed430a3c2c4de20e3c697e1f5e3f9ebdb4
abdouxone <abdouselim.edu@gmail.com>
2026-09-10 16:37:49 +0100

    improve the cookie4

--- app/cookies/cookie5/page.tsx
+++ app/cookies/cookie5/page.tsx
@@ -5,9 +5,9 @@ import { useState } from "react";
 export default function Cookie() {
   const [closed, setClosed] = useState(false);
   return (
-    <div className="items-center flex-1 bg-linear-to-r from-purple-200 via-red-100 to-cyan-100 justify-center flex">
+    <div className="items-center h-screen bg-linear-to-r from-purple-200 via-red-100 to-cyan-100 justify-center flex">
       {!closed && (
-        <div className="max-w-sm w-full items-center py-3 px-5 fixed  bottom-4 justify-between bg-white shadow-xl rounded-full flex">
+        <div className="max-w-sm w-full items-center p-3 fixed  bottom-4 justify-between bg-white shadow-sm border border-gray-100 rounded-full flex">
           <div className="flex items-center gap-3">
             <img
               src="/cookies/images/cookie/img-1.png"
@@ -18,11 +18,13 @@ export default function Cookie() {
               The website uses cookies.
             </p>
           </div>
-          <XIcon
-            onClick={() => setClosed(true)}
-            className="bg-gray-100 rounded-full cursor-pointer"
-            size={30}
-          />
+          <button className=" bg-gray-100  rounded-full  cursor-pointer transition-all p-1">
+            <XIcon
+              onClick={() => setClosed(true)}
+              size={30}
+              className="hover:scale-110  duration-500 overflow-hidden hover:rotate-45"
+            />
+          </button>
         </div>
       )}
     </div>
